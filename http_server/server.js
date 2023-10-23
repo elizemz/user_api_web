ca2b2ced232e848876473cc998025d358f5867c1
ugtakhbayar surakhbayar <ugtakhbayars@gmail.com>
2023-10-23 21:22:10 +0800

    Add Express js and users.json file

---
+++ http_server/server.js
@@ -0,0 +1,20 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("REQ", req.method);
+  if (req.method === "GET") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+
+    res.end("<h1>GET METHOD</h1>");
+  } else if (req.method === "POST") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+
+    res.end("<h1>POST METHOD</h1>");
+  }
+});
+
+server.listen(8008, () => {
+  console.log("Listening on port 8008");
+});
