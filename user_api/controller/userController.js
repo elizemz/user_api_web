c682cbf69ee7760adc6feef7e13e6199d8243eee
ugtakhbayar surakhbayar <ugtakhbayars@gmail.com>
2023-10-30 18:57:10 +0800

    add file utils function

--- user_api/controller/userController.js
+++ user_api/controller/userController.js
@@ -1,19 +1,15 @@
 const { v4: uuidv4 } = require("uuid");
-const fs = require("fs");
+const { readFile, writeFile } = require("../utils/fileHandler");
 
 const getAllUser = (req, res) => {
-  console.log("Get all users");
-  const { users } = JSON.parse(
-    fs.readFileSync("users.json", { encoding: "utf-8" })
-  );
+  console.log("Get all `users");
+  const users = readFile("users.json");
   res.status(200).json({ message: "success", users });
 };
 
 const getUserById = (req, res) => {
   console.log("Get an user by ID");
-  const { users } = JSON.parse(
-    fs.readFileSync("users.json", { encoding: "utf-8" })
-  );
+  const users = readFile("users.json");
   const findUser = users.filter((user) => user.id === req.params.userId);
   if (findUser.length === 0) {
     res.status(400).json({ message: `${userId} тай хэрэглэгч олдсонгүй.` });
@@ -25,34 +21,22 @@ const getUserById = (req, res) => {
 const createUser = (req, res) => {
   console.log("Create new user");
   const newUser = { id: uuidv4(), ...req.body };
-
-  const { users } = JSON.parse(
-    fs.readFileSync("users.json", { encoding: "utf-8" })
-  ); // {users: []}
-
+  const users = readFile("users.json");
   users.push(newUser);
-
-  fs.writeFileSync("users.json", JSON.stringify({ users }), {
-    encoding: "utf-8",
-  });
+  writeFile("users.json", users);
   res.status(201).json({ message: "success" });
 };
 
 const updateUserById = (req, res) => {
   console.log("Update user by id");
   const { userId } = req.params;
-  const { users } = JSON.parse(
-    fs.readFileSync("users.json", { encoding: "utf-8" })
-  );
+  const users = readFile("users.json");
   let index = users.findIndex((user) => user.id === userId);
-
   if (index === -1) {
     res.status(400).json({ message: `${userId} тай хэрэглэгч олдсонгүй.` });
   } else {
     users[index] = { ...users[index], ...req.body };
-    fs.writeFileSync("users.json", JSON.stringify({ users }), {
-      encoding: "utf-8",
-    });
+    writeFile("users.json", users);
     res.status(200).json({
       message: `${userId} тай хэрэглэгчийг шинэчиллээ.`,
       user: users[index],
@@ -63,18 +47,13 @@ const updateUserById = (req, res) => {
 const deleteUserById = (req, res) => {
   console.log("Delete user by id");
   const { userId } = req.params;
-  const { users } = JSON.parse(
-    fs.readFileSync("users.json", { encoding: "utf-8" })
-  );
+  const users = readFile("users.json");
   const index = users.findIndex((el) => el.id === userId);
   if (index < 0) {
     res.status(400).json({ message: `${userId} тай хэрэглэгч олдсонгүй.` });
   } else {
     users.splice(index, 1);
-
-    fs.writeFileSync("users.json", JSON.stringify({ users }), {
-      encoding: "utf-8",
-    });
+    writeFile("users.json", users);
     res.status(200).json({ message: `${userId} тай хэрэглэгч устгалаа.` });
   }
 };
